d148b908cf767bb1e781f5dbfa61319f0b74dc54
vistafan12 <kroljestboss@o2.pl>
2017-02-07 21:58:42 +0100

    'add'

---
+++ wp-content/themes/rokophoto-lite/inc/customizer/customizer-site-logo.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Add the site logo section to customizer.
+
+ * @package RokoPhoto
+ */
+
+/**
+ * Hook the logo controls to the customizer.
+ *
+ * @param object $wp_customize The wp_customize object.
+ */
+function rokophotolite_site_logo_customize_register( $wp_customize ) {
+
+	$wp_customize->add_section( 'rokophotolite_logo_section', array(
+		'priority' => 25,
+		'title'    => __( 'Site Logo', 'rokophotolite' ),
+	) );
+
+	$wp_customize->add_setting( 'rokophotolite_logo_image', array(
+		'default'           => get_template_directory_uri() . '/img/logo.png',
+		'capability'        => 'edit_theme_options',
+		'sanitize_callback' => 'esc_url_raw',
+	) );
+
+	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'rokophotolite_logo_image', array(
+		'label'    => __( 'Site Logo Image', 'rokophotolite' ),
+		'section'  => 'rokophotolite_logo_section',
+		'priority' => 5,
+		'settings' => 'rokophotolite_logo_image',
+	) ) );
+}
+
+add_action( 'customize_register', 'rokophotolite_site_logo_customize_register' );
---
+++ wp-content/themes/rokophoto-lite/inc/customizer/customizer-subheader.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Subheader section customizer controls.
+ *
+ * @package RokoPhoto
+ */
+
+/**
+ * Hook the subheader section controls to the customizer.
+ *
+ * @param object $wp_customize The wp_customize object.
+ */
+function rokophotolite_subheader_customize_register( $wp_customize ) {
+
+	/**
+	 * Sub header
+	 */
+
+	$wp_customize->add_section('rokophotolite_subhead_section', array(
+		'priority' => 60,
+		'title' => __( 'Sub-Header', 'rokophotolite' ),
+	));
+
+	$wp_customize->add_setting('rokophotolite_subhead_title', array(
+		'default' => __( 'Welcome to', 'rokophotolite' ),
+		'capability' => 'edit_theme_options',
+		'sanitize_callback' => 'sanitize_text_field',
+		'transport' => 'postMessage',
+	));
+
+	$wp_customize->add_control('rokophotolite_subhead_title', array(
+		'label' => __( 'Sub-Header Title', 'rokophotolite' ),
+		'section' => 'rokophotolite_subhead_section',
+		'priority' => 5,
+		'settings' => 'rokophotolite_subhead_title',
+	));
+
+}
+
+add_action( 'customize_register', 'rokophotolite_subheader_customize_register' );
